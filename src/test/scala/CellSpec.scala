ea0e5e5b3b51ac7312bb06cb50113a8fe414b8cf
jrudnick <Jack_Rudnick@cable.comcast.com>
2011-07-16 16:30:55 -0400

    Cell registration test

--- src/test/scala/CellSpec.scala
+++ src/test/scala/CellSpec.scala
@@ -5,20 +5,30 @@ import org.scalatest.{WordSpec, BeforeAndAfterAll}
 import akka.actor.{ActorRef, Actor}
 import akka.actor.Actor._
 import akka.testkit.TestKit
+import akka.util.duration._
 
 class CellSpec extends WordSpec with BeforeAndAfterAll with ShouldMatchers with TestKit {
-  val controller = actorOf(new ControllerStub).start()
+  val controller = actorOf(new ControllerStub(testActor)).start()
   val board = actorOf(new BoardStub).start()
-  val cell = actorOf(new Cell(0,0,controller,board)).start()
+  val cell = actorOf(new Cell(0,0,controller,board))
 
   override protected def afterAll() {
     cell.stop()
   }
+
+  "A Cell" should {
+    "Send a registration to the controller" in {
+      within(1000 millis) {
+        cell.start()
+        expectMsg(CellRegistration(0,0))
+      }
+    }
+  }
 }
 
-class ControllerStub extends Actor {
+class ControllerStub(testActor:ActorRef) extends Actor {
   def receive = {
-    case _ => None
+    case msg => testActor ! msg
   }
 }
 
