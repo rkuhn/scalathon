2129f70478b98e1656129727e5fd8f76a34b6519
jrudnick <Jack_Rudnick@cable.comcast.com>
2011-07-16 15:49:39 -0400

    commiting src/test

---
+++ src/test/scala/CellSpec.scala
@@ -0,0 +1,29 @@
+package akka.tutorial.conway
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.{WordSpec, BeforeAndAfterAll}
+import akka.actor.{ActorRef, Actor}
+import akka.actor.Actor._
+import akka.testkit.TestKit
+
+class CellSpec extends WordSpec with BeforeAndAfterAll with ShouldMatchers with TestKit {
+  val controller = actorOf(new ControllerStub).start()
+  val board = actorOf(new BoardStub).start()
+//  val cell = actorOf(new Cell(0,0,controller,board)).start()
+
+  override protected def afterAll() {
+//    cell.stop()
+  }
+}
+
+class ControllerStub extends Actor {
+  def receive = {
+    case _ => None
+  }
+}
+
+class BoardStub extends Actor {
+  def receive = {
+    case _ => None
+  }
+}
