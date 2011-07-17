2efba98140925b18c1b96403ddf0ab0743c8bc21
Artie Pesh-Imam <apeshimam@gmail.com>
2011-07-17 14:56:53 -0400

    Removing printlns

--- src/main/scala/akka/tutorials/conway/Board.scala
+++ src/main/scala/akka/tutorials/conway/Board.scala
@@ -10,22 +10,18 @@ class Board(xSize:Int, ySize:Int, displayRef:ActorRef) extends Actor{
   val messageCountPerRound = Map[Int, Int]()  
   
   def createBoard():Array[Array[Boolean]] =  { 
-    println("creating board")
     Array.ofDim[Boolean](xSize, ySize)    
   }
 
   def receive = {
     // Handle the CellToBoard message
     case CellToBoard(alive:Boolean, round:Int, x:Int, y:Int) => {
-      println("received CellToBoard")
       if (boardList.size <= round)
         boardList = boardList :+ createBoard()
       // Set the alive or dead
       boardList(round)(x)(y) = alive
 
       messageCountPerRound += round -> (messageCountPerRound.getOrElse(round, 0) + 1)
-      
-      println("messageCountPerRound: " + messageCountPerRound(round))
       
       if(messageCountPerRound(round) == xSize * ySize) {
           println("In if.. going to send message")
