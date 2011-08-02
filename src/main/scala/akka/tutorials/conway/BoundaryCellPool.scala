cb5ab9ed6a46d88d4a02635807baf1d90186422c
Artie Pesh-Imam <apeshimam@gmail.com>
2011-08-02 11:05:45 -0400

    Added ActorPooling for BoundaryCells

---
+++ src/main/scala/akka/tutorials/conway/BoundaryCellPool.scala
@@ -0,0 +1,28 @@
+package akka.tutorials.conway
+
+import akka.actor.Actor
+import akka.actor.Actor._
+import akka.routing._
+
+class BoundaryCellPool extends Actor with DefaultActorPool
+                                     with DefaultActorPoolSupervisionConfig
+                                     with BoundedCapacityStrategy
+                                     with SmallestMailboxSelector
+                                     with MailboxPressureCapacitor
+                                     with Filter
+                                      with RunningMeanBackoff
+                                      with BasicRampup
+{
+  def receive = _route
+  def lowerBound = 1
+  def upperBound = 5
+  def pressureThreshold = 1
+  def partialFill = true
+  def selectionCount = 1
+  def rampupRate = 0.1
+  def backoffRate = 0.50
+  def backoffThreshold = 0.50
+  def instance = actorOf(new BoundaryCell)
+  
+}
+                          
