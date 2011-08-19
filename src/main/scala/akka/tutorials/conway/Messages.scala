801cc84e0922b85b16fa3d09eff62e9710cff253
Artie Pesh-Imam <apeshimam@gmail.com>
2011-08-19 00:59:30 -0400

    Fixed BoardSpec test Issue

--- src/main/scala/akka/tutorials/conway/Messages.scala
+++ src/main/scala/akka/tutorials/conway/Messages.scala
@@ -1,41 +1,64 @@
 package akka.tutorials.conway
 
 import akka.actor.ActorRef
+import collection.mutable.StringBuilder
 
-case class ControllerToCellInitialize(alive:Boolean, neighbors:Array[ActorRef])
+case class ControllerToCellInitialize(alive: Boolean, neighbors: Array[ActorRef])
+
 case object ControllerInitialize
+
 case object ControllerToCellStart
-case object ControllerToCellStop 
+
+case object ControllerToCellStop
+
 case object ControllerStart
 
 case object BoardToControllerAdvanceRound
 
-case class CellRegistration(x:Int, y:Int)
-case class CellToCell(alive:Boolean, round:Int)
-case class CellToBoard(alive:Boolean, round:Int, x:Int, y:Int)
-case class RequestBoardState(round:Int)
+case class CellRegistration(x: Int, y: Int)
 
-case class BoardState(round:Int, boardState:Array[Array[Boolean]]) {
-  override def equals(other: Any) : Boolean = {
+case class CellToCell(alive: Boolean, round: Int)
+
+case class CellToBoard(alive: Boolean, round: Int, x: Int, y: Int)
+
+case class RequestBoardState(round: Int)
+
+case class BoardState(round: Int, boardState: Array[Array[Boolean]]) {
+
+  override def toString() = {
+    val builder = new StringBuilder()
+    builder.append("\n[Round: " + round)
+
+    builder.append("\n")
+    for (i <- 0 until boardState.length) {
+      for (j <- 0 until boardState.length) {
+        builder.append("Board[%d,%d]=%b".format(i, j, boardState(i), (j)))
+        builder.append("\n")
+      }
+    }
+    builder.toString()
+  }
+
+  override def equals(other: Any): Boolean = {
     other match {
       case state: BoardState => {
-        val boardStateFlat = boardState.flatten
-        val otherBoardStateFlat = state.boardState.flatten
         var isEqual = true
-        
-        if( (boardState(0).size != state.boardState(0).size) ||  (boardState.length != state.boardState.length) )
+
+        if ((boardState(0).size != state.boardState(0).size) || (boardState.length != state.boardState.length))
           return false
-          
-        for(i <- 0 until boardState.length) {
-          for(j <- 0 until boardState.length) {
-            isEqual = boardState(i)(j) == state.boardState(i)(j)
+
+          for (i <- 0 until boardState.length) {
+            for (j <- 0 until boardState.length) {
+              isEqual = boardState(i)(j) == state.boardState(i)(j)
+              if(!isEqual)
+                return false
+            }
           }
-        }
-        
-        (round == state.round && isEqual) 
-      } 
+
+        true
+      }
       case _ => false
     }
-    
+
   }
 }
--- src/test/scala/BoardSpec.scala
+++ src/test/scala/BoardSpec.scala
@@ -10,75 +10,76 @@ import akka.util.duration._
 import akka.tutorials.conway._
 
 class BoardSpec extends WordSpec with BeforeAndAfterAll with ShouldMatchers with TestKit {
-    //tests to add
-    //verify that the board to be renedered is correct
-    
-    private def createBoard(xSize: Int, ySize: Int,  alive: Boolean): Array[Array[Boolean]] = {
-      val board = Array.ofDim[Boolean](xSize, ySize)
-      for(i <- 0 until xSize) {
-        for(j <- 0 until ySize ) {
-          board(i)(j) = alive
-        }
-      }
-      return board
-    }
-    
-    "A BoardActor" should {
+  //tests to add
+  //verify that the board to be renedered is correct
 
-      "try to render a board if the BoardToControllerDisplayRound returns true" in {
-        
-        val controllerStub = actorOf(new ControllerStub(testActor, true)).start()        
-        val boardActor = actorOf(new Board(1, 1, testActor, controllerStub,5)).start()
-        
-        within (2000 millis) {
-          boardActor !  CellToBoard(true, 0 , 0, 0)
-          expectMsg(BoardState(0,createBoard(1,1,true)))
-        }
+  private def createBoard(xSize: Int, ySize: Int, alive: Boolean): Array[Array[Boolean]] = {
+    val board = Array.ofDim[Boolean](xSize, ySize)
+    for (i <- 0 until xSize) {
+      for (j <- 0 until ySize) {
+        board(i)(j) = alive
       }
-      
-      "send a message to the display actor if the board is complete." in {
-        val controllerStub = actorOf(new ControllerStub(testActor, true)).start()
-        val boardActor = actorOf(new Board(3, 3, testActor, controllerStub,5)).start()
-        
-        within (3000 millis) {
-          for (i <- 1 to 9) {
-            val response = boardActor ! CellToBoard(false, 0, 0, 0)
+    }
+
+    return board
+  }
+
+  "A BoardActor" should {
+
+    "try to render a board if the BoardToControllerDisplayRound returns true" in {
+
+      val controllerStub = actorOf(new ControllerStub(testActor, true)).start()
+      val boardActor = actorOf(new Board(1, 1, testActor, controllerStub, 5)).start()
+
+      within(2000 millis) {
+        boardActor ! CellToBoard(true, 0, 0, 0)
+        expectMsg(BoardState(0, createBoard(1, 1, true)))
+      }
+    }
+
+    "send a message to the display actor if the board is complete." in {
+      val controllerStub = actorOf(new ControllerStub(testActor, true)).start()
+      val boardActor = actorOf(new Board(3, 3, testActor, controllerStub, 5)).start()
+
+      within(3000 millis) {
+        for (i <- 0 until 3) {
+          for (j <- 0 until 3) {
+            val response = boardActor ! CellToBoard(false, 0, i, j)
           }
-          expectMsg(BoardState(0, createBoard(3,3,false)))
-          
         }
+        expectMsg(BoardState(0, createBoard(3, 3, false)))
+
       }
-      "send not message to the display actor if the board is complete." in {
-        val controllerStub = actorOf(new ControllerStub(testActor, true)).start()
-        val boardActor = actorOf(new Board(3, 3, testActor, controllerStub,5)).start()
-        
-        within (3000 millis) {
-          for (i <- 1 to 6) {
-            val response = boardActor ! CellToBoard(false, 0, 0, 0)
+    }
+    "send not message to the display actor if the board is complete." in {
+      val controllerStub = actorOf(new ControllerStub(testActor, true)).start()
+      val boardActor = actorOf(new Board(3, 3, testActor, controllerStub, 5)).start()
+
+      within(3000 millis) {
+        for (i <- 1 to 6) {
+          val response = boardActor ! CellToBoard(false, 0, 0, 0)
+        }
+        expectNoMsg
+
+      }
+    }
+    "send a board that shows all cells are alive if every message received indicates that a cell is alive." in {
+      val controllerStub = actorOf(new ControllerStub(testActor, true)).start()
+      val boardActor = actorOf(new Board(3, 3, testActor, controllerStub, 5)).start()
+
+      within(5000 millis) {
+        for (i <- 0 until 3) {
+          for (j <- 0 until 3) {
+            val response = boardActor ! CellToBoard(true, 0, i, j)
           }
-         expectNoMsg
-          
         }
+        expectMsg(BoardState(0, createBoard(3, 3, true)))
       }
-      "send a board that shows all cells are alive if every message received indicates that a cell is alive." in {
-        val controllerStub = actorOf(new ControllerStub(testActor, true)).start()
-        val boardActor = actorOf(new Board(3, 3, testActor, controllerStub,5)).start()
-        val board = createBoard(3,3, true)
-        
-        within (3000 millis) {
-          for (i <- 0 to 2) {
-            for(j <- 0 to 2) {  
-              val response = boardActor ! CellToBoard(true, 0, i, j)
-            }
-          }
-            
-         expectMsg(BoardState(0,board))
-        }
-      }
-    }    
+    }
+  }
 }
 
-class ControllerStub(testActor:ActorRef, returnValue: Boolean) extends Actor {
+class ControllerStub(testActor: ActorRef, returnValue: Boolean) extends Actor {
   def receive = {
     case _ => self.reply(returnValue)
   }
